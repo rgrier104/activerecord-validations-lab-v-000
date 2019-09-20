f792d49e36dadce3a26e08acadeecd786e693c30
Rachel Grier <rgrier104@gmail.com>
2019-09-20 14:43:51 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
+class MyValidator < ActiveModel::Validator
+  def validate(record)
+    unless record.title.include?("Won't Believe","Secret","Top","Guess")
+      record.errors[:title] << 'Need a clickbait-y title'
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w{Fiction Non-Fiction}}
+  validates_with MyValidator
 end
