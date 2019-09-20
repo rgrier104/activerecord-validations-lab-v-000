88cc9d7823eb20df0af0f9e2ccc33b05e534e86e
Rachel Grier <rgrier104@gmail.com>
2019-09-20 15:07:42 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,15 @@
-class MyValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.title.include?("Won't Believe","Secret","Top","Guess")
-      record.errors[:title] << 'Need a clickbait-y title'
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\b(Won't Believe|Secret|Top|Guess)\b/i
+      record.errors[attribute] << (options[:message] || "is not clickbait-y")
     end
   end
 end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates :title, presence: true
+  validates :title, presence: true, 
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w{Fiction Non-Fiction}}
-  validates_with MyValidator
 end
