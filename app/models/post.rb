49fb4efee98cc1955a9b84961c072504e95152af
Rachel Grier <rgrier104@gmail.com>
2019-09-20 15:07:55 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ end
 
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates :title, presence: true,
+  validates :title, presence: true, title: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w{Fiction Non-Fiction}}
